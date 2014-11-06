765b2622b160eb5724c760aee3e6c1ac79bab637
damien <damdam.gold@gmail.com>
2014-11-06 03:21:58 +0100

    Add the route system based on ngRoute & the language system (fr & en)

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,8 +1,16 @@
 (function()
 {
     var portfolio = angular.module('portfolio', ["ngAnimate", 'ui.bootstrap', 'ngRoute']);
-    var lang = "en"
-    /*portfolio.config(function($routeProvider, $locationProvider)
+    /*Dev Mode : No cache*/
+    portfolio.run(function($rootScope, $templateCache)
+                    {
+                       $rootScope.$on('$viewContentLoaded', function()
+                                                               {
+                                                                  $templateCache.removeAll();
+                                                               });
+                    });
+
+    portfolio.config(function($routeProvider, $locationProvider)
     {
         $routeProvider
             .when('/', {
@@ -11,10 +19,6 @@
                     })
             .when('/Welcome', {
                         templateUrl: 'templates/welcome.html',
-                        controller: 'PortfolioCtrl'
-                    })
-            .when('/AboutMe', {
-                        templateUrl: 'templates/aboutme.html',
                         controller: 'PortfolioCtrl'
                     })
             .when('/Edu', {
@@ -33,15 +37,284 @@
                         templateUrl: 'templates/contact.html',
                         controller: 'PortfolioCtrl'
                     })
-            .otherwise({redirectTo: '/'});
-  });*/
+            .when('/Pro', {
+                        templateUrl: 'templates/projects/proproject.html',
+                        controller: 'PortfolioCtrl'
+                    })
+            .when('/Stud', {
+                        templateUrl: 'templates/projects/studproject.html',
+                        controller: 'PortfolioCtrl'
+                    })
+            .when('/Perso', {
+                        templateUrl: 'templates/projects/persoproject.html',
+                        controller: 'PortfolioCtrl'
+                    })
+            .when('/WemanityProject', {
+                        templateUrl: 'templates/projects/wemanityproject.html',
+                        controller: 'PortfolioCtrl'
+                    })
+            .when('/CAMS', {
+                templateUrl: 'templates/skills/business/cams.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/DevOps', {
+                templateUrl: 'templates/skills/business/devops.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/TimeToMarket', {
+                templateUrl: 'templates/skills/business/timetomarket.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/CD', {
+                templateUrl: 'templates/skills/business/cd.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/CI', {
+                templateUrl: 'templates/skills/business/ci.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Improvement', {
+                templateUrl: 'templates/skills/business/improvement.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Scrum', {
+                templateUrl: 'templates/skills/business/scrum.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/XP', {
+                templateUrl: 'templates/skills/business/xp.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Kanban', {
+                templateUrl: 'templates/skills/business/kanban.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Lean', {
+                templateUrl: 'templates/skills/business/lean.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/LeanStartUp', {
+                templateUrl: 'templates/skills/business/leanstartup.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Waterfall', {
+                templateUrl: 'templates/skills/business/waterfall.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/StrongTeamSpirit', {
+                templateUrl: 'templates/skills/business/strongteamspirit.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/OrganisedAndApplied', {
+                templateUrl: 'templates/skills/business/organisedandapplied.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/AutonomousAndForceProposal', {
+                templateUrl: 'templates/skills/business/autonomousandforceproposal.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/AbilityToAdapt', {
+                templateUrl: 'templates/skills/business/abilitytoadapt.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Linux', {
+                templateUrl: 'templates/skills/technical/os-network/linux.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Windows', {
+                templateUrl: 'templates/skills/technical/os-network/windows.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Juniper', {
+                templateUrl: 'templates/skills/technical/os-network/juniper.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Cisco', {
+                templateUrl: 'templates/skills/technical/os-network/cisco.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/OSI', {
+                templateUrl: 'templates/skills/technical/os-network/osi.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/TCPIP', {
+                templateUrl: 'templates/skills/technical/os-network/tcpip.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Netfilter', {
+                templateUrl: 'templates/skills/technical/os-network/netfilter.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Vsphere', {
+                templateUrl: 'templates/skills/technical/os-network/vsphere.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/ITSecurity', {
+                templateUrl: 'templates/skills/technical/os-network/itsecurity.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Python', {
+                templateUrl: 'templates/skills/technical/languages/python.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Puppet', {
+                templateUrl: 'templates/skills/technical/languages/puppet.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/C', {
+                templateUrl: 'templates/skills/technical/languages/c.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Perl', {
+                templateUrl: 'templates/skills/technical/languages/perl.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/JavaScript', {
+                templateUrl: 'templates/skills/technical/languages/js.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/PHP', {
+                templateUrl: 'templates/skills/technical/languages/php.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/SQL', {
+                templateUrl: 'templates/skills/technical/languages/sql.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/HTMLCSS', {
+                templateUrl: 'templates/skills/technical/languages/htmlcss.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Fabric', {
+                templateUrl: 'templates/skills/technical/framework/fabric.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Scapy', {
+                templateUrl: 'templates/skills/technical/framework/scapy.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Django', {
+                templateUrl: 'templates/skills/technical/framework/django.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/AngularJS', {
+                templateUrl: 'templates/skills/technical/framework/angularjs.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/JQuery', {
+                templateUrl: 'templates/skills/technical/framework/jquery.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/D3', {
+                templateUrl: 'templates/skills/technical/framework/d3.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/RaphaelJS', {
+                templateUrl: 'templates/skills/technical/framework/raphaeljs.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Git', {
+                templateUrl: 'templates/skills/tools/scm/git.html',
+                controller: 'PortfolioCtrl'
+            })
+             .when('/GitIHM', {
+                templateUrl: 'templates/skills/tools/scm/gitihm.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/SVN', {
+                templateUrl: 'templates/skills/tools/scm/svn.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Docker', {
+                templateUrl: 'templates/skills/tools/devops/docker.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Jenkins', {
+                templateUrl: 'templates/skills/tools/devops/jenkins.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/DeployIT', {
+                templateUrl: 'templates/skills/tools/devops/deployit.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Ant', {
+                templateUrl: 'templates/skills/tools/build/ant.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Maven', {
+                templateUrl: 'templates/skills/tools/build/maven.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Make', {
+                templateUrl: 'templates/skills/tools/build/make.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Nmap', {
+                templateUrl: 'templates/skills/tools/it-security/nmap.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Nessus', {
+                templateUrl: 'templates/skills/tools/it-security/nessus.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Wireshark', {
+                templateUrl: 'templates/skills/tools/it-security/wireshark.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Tshark', {
+                templateUrl: 'templates/skills/tools/it-security/tshark.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/IPTraf', {
+                templateUrl: 'templates/skills/tools/it-security/iptraf.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Netcat', {
+                templateUrl: 'templates/skills/tools/it-security/netcat.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/ZenOSS', {
+                templateUrl: 'templates/skills/tools/supervision/zenoss.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Redmine', {
+                templateUrl: 'templates/skills/tools/project-management/redmine.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Trello', {
+                templateUrl: 'templates/skills/tools/project-management/trello.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Gantter', {
+                templateUrl: 'templates/skills/tools/project-management/gantter.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/Jira', {
+                templateUrl: 'templates/skills/tools/project-management/jira.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/MSProject', {
+                templateUrl: 'templates/skills/tools/project-management/msproject.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/ItiProject', {
+                templateUrl: 'templates/projects/itiproject.html',
+                controller: 'PortfolioCtrl'
+            })
+            .when('/EpitechProject', {
+                templateUrl: 'templates/projects/epitechproject.html',
+                controller: 'PortfolioCtrl'
+            })
+            .otherwise({redirectTo: '/'
+                        });
+  });
 
     portfolio.factory("Share", function()
                                 {
                                     return {
                                         "header":1,
-                                        "anim":0,
-                                        "view":""
+                                        "lang":"en"
+
                                     };
                                 });
 
@@ -61,54 +334,27 @@
     portfolio.controller("MenuCtrl", function($scope, Share, $http, $rootScope)
                                     {
                                         $scope.menuFixed = "";
+                                        $scope.langH = Share.lang;
                                         $scope.$on('UpdateMenu', function()
                                                                         {
                                                                             console.log("toto");
                                                                             $scope.menuFixed = Share.header == 1 ? "" : "menu-fixed";
                                                                         }, true);
 
-                                        $scope.getPage = function(id)
-                                                            {
-                                                                $http({
-                                                                    url:"templates/"+(id == 0 ? "welcome" : id == 1 ? "aboutme" : id == 2 ? "edu" : id == 3 ? "xp" : id == 4 ? "skills" : id == 5 ? "contact" : "welcome")+".html",
-                                                                    method:"POST",
-                                                                    headers:{"Content-Type":"application/X-WWW-form-urlencoded"},
-                                                                    data:{"id":id}
-                                                                }).success(function(data)
-                                                                            {
-                                                                                console.log(data);
-                                                                                Share.view = data
-                                                                                $rootScope.$broadcast("UpdateViews");
-                                                                            }).error(function()
-                                                                                    {
-                                                                                        alert("fail")
-                                                                                    });
-                                                            }
-
+                                        $scope.lang = function(t)
+                                                        {
+                                                            Share.lang = (t == 0 ? "en" : "fr");
+                                                            $scope.langH = Share.lang;
+                                                            $rootScope.$broadcast("UpdateLang");
+                                                        };
                                     });
 
-    portfolio.controller("PortfolioCtrl", function($scope, Share, $anchorScroll, $location, $sce)
+    portfolio.controller("PortfolioCtrl", function($scope, Share)
                                         {
-                                            $scope.anim = '';
-                                            //$scope.view = Share.view;
-                                            $scope.$on('UpdateViews', function()
+                                            $scope.lang = Share.lang;
+                                            $scope.$on('UpdateLang', function()
                                                                         {
-                                                                            console.log("toto");
-                                                                            $scope.view = $sce.trustAsHtml(Share.view);
-                                                                        });
-                                            $scope.goToAnchor = function(x)
-                                                                {
-                                                                    var newHash = 'anchor' + x;
-                                                                    if ($location.hash() !== newHash)
-                                                                    {
-                                                                        console.log($location.hash());
-                                                                        $location.hash('anchor' + x);
-                                                                        console.log(window.scrollX);
-                                                                    }
-                                                                    else
-                                                                    {
-                                                                        $anchorScroll();
-                                                                    }
-                                                                };
+                                                                            $scope.lang = Share.lang;
+                                                                        }, true);
                                         });
 })();
